623b8bcc2e9e74682625cd2540d2a1982146c5e6
martin <martin.hinz@ufg.uni-kiel.de>
2016-06-06 11:27:27 +0200

    made pathnames space-secure, added unoconv conversion for 'doc' filetype

--- prepare_for_peer_review.sh
+++ prepare_for_peer_review.sh
@@ -10,12 +10,18 @@ rm $OUTPUT_DIR/* -r
 
 for file in $INPUT_DIR/$MANUSCRIPT_PREFIX*
 do
-if [ -f $file ] ; then
+if [ -f "$file" ] ; then
  echo "Processing $file"
- basename=$(basename $file)
+ basename=$(basename "$file")
  mainfile=$OUTPUT_DIR/$basename-all.md
  mdfile="$OUTPUT_DIR/${basename%.*}.md"
  outfile="$OUTPUT_DIR/${basename%.*}.pdf"
+ if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
+ then
+   echo "docfile"
+   unoconv -f docx "$file"
+   file="$INPUT_DIR/${basename%.*}.docx"
+ fi
  touch $mdfile
  printf "## Manuscript\n\n" > $mdfile
  echo "$(pandoc -t markdown ""$file"")" >> $mdfile
@@ -28,11 +34,17 @@ done
 
 for file in $INPUT_DIR/$CAPTION_PREFIX*
 do
- if [ -f $file ] ; then
+ if [ -f "$file" ] ; then
  echo "Processing $file"
- basename=$(basename $file)
+ basename=$(basename "$file")
  mdfile="$OUTPUT_DIR/${basename%.*}.md"
  outfile="$OUTPUT_DIR/${basename%.*}.pdf"
+ if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
+ then
+   echo "docfile"
+   unoconv -f docx "$file"
+   file="$INPUT_DIR/${basename%.*}.docx"
+ fi
  touch $mdfile
  printf "\n\n\pagebreak\n\n## Captions\n\n" > $mdfile
  printf "$(pandoc -t markdown ""$file"")" >> $mdfile
@@ -45,26 +57,32 @@ done
 COUNTER=0
 for file in "$INPUT_DIR"/"$TABLES_PREFIX"*
 do
- if [ -f $file ] ; then
+ if [ -f "$file" ] ; then
  let COUNTER=COUNTER+1 
  echo "Processing $file"
- basename=$(basename $file)
+ basename=$(basename "$file")
  htmlfile="$OUTPUT_DIR/${basename%.*}.html"
  outfile_pdf="$OUTPUT_DIR/${basename%.*}.pdf"
 # printf "\n\pagebreak\n\n# Table $((i+1))\n\n" > $outfile
+ if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
+ then
+   echo "docfile"
+   unoconv -f docx "$file"
+   file="$INPUT_DIR/${basename%.*}.docx"
+ fi
  pandoc "$file" -o "$htmlfile"
- pandoc "$htmlfile" -o "$outfile_pdf" --latex-engine=xelatex -H $SETTINGS_DIR/options_table.sty
+ pandoc "$htmlfile" -o "$outfile_pdf" --latex-engine=xelatex -H "$SETTINGS_DIR"/options_table.sty
  outfile_md="$OUTPUT_DIR/${basename%.*}.md"
- touch $outfile_md
- printf "\n\n\pagebreak\n\n## Table  $COUNTER: ${basename:5}\n\n" > $outfile_md
- mkdir $outfile_pdf.dir
- pdftk $outfile_pdf burst output $outfile_pdf.dir/page_%03d.pdf
- rm $outfile_pdf.dir/doc_data.txt
- for file in $outfile_pdf.dir/*
+ touch "$outfile_md"
+ printf "\n\n\pagebreak\n\n## Table  $COUNTER: ${basename:5}\n\n" > "$outfile_md"
+ mkdir "$outfile_pdf.dir"
+ pdftk "$outfile_pdf" burst output "$outfile_pdf.dir"/page_%03d.pdf
+ rm "$outfile_pdf.dir"/doc_data.txt
+ for file in "$outfile_pdf.dir"/*
  do
-  printf "![$basename]($file)\ \n\n\pagebreak" >> $outfile_md 
+  printf "![$basename]($file)\ \n\n\pagebreak" >> "$outfile_md"
  done
- cat $outfile_md >> $mainfile
+ cat "$outfile_md" >> $mainfile
 fi
 done
 
@@ -72,12 +90,12 @@ done
 COUNTER=0
 for file in "$INPUT_DIR"/"$FIGURE_PREFIX"*
 do
-if [ -f $file ] ; then
+if [ -f "$file" ] ; then
  let COUNTER=COUNTER+1 
  echo "Processing $file"
- basename=$(basename $file)
+ basename=$(basename "$file")
  outfile_img="$OUTPUT_DIR/${basename%.*}.pdf"
- $(convert $file $outfile_img)
+ $(convert "$file" $outfile_img)
  outfile_md="$OUTPUT_DIR/${basename%.*}.md"
  touch $outfile_md
  printf "\n\n\pagebreak\n\n## Figure $COUNTER: ${basename:5}\n\n" > $outfile_md
