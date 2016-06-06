5e4f5748acf6f13c9cc313476bdcdad4a67df480
martin <martin.hinz@ufg.uni-kiel.de>
2016-06-06 14:48:07 +0200

    sanitize output file names, put converted tables in output instead input directory

--- prepare_for_peer_review.sh
+++ prepare_for_peer_review.sh
@@ -1,55 +1,56 @@
 #/bim/bash
 
 # Reading Config
-echo "Reading config...." >&2
+echo "Reading config...."
 source settings/config
 
 # Clear output
-rm $OUTPUT_DIR/* -r
+rm "$OUTPUT_DIR"/* -r
 # Manuscript
 
-for file in $INPUT_DIR/$MANUSCRIPT_PREFIX*
+for file in "$INPUT_DIR"/"$MANUSCRIPT_PREFIX"*
 do
 if [ -f "$file" ] ; then
  echo "Processing $file"
- basename=$(basename "$file")
- mainfile=$OUTPUT_DIR/$basename-all.md
+ basename=$( echo $(basename "$file") |  sed 's/[^A-Za-z0-9_.]/-/g;s/-*-/-/g;s/^-//;s/-$//;s/-\././g' )
+ mainfile="$OUTPUT_DIR/$basename-all.md"
  mdfile="$OUTPUT_DIR/${basename%.*}.md"
  outfile="$OUTPUT_DIR/${basename%.*}.pdf"
  if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
  then
    echo "converting to docx"
-   unoconv -f docx "$file"
-   file="$INPUT_DIR/${basename%.*}.docx"
+   unoconv -f docx -o "$OUTPUT_DIR/${basename%.*}.docx" "$file"
+   file="$OUTPUT_DIR/${basename%.*}.docx"
  fi
- touch $mdfile
- printf "## Manuscript\n\n" > $mdfile
- echo "$(pandoc -t markdown ""$file"")" >> $mdfile
- cat $mdfile > $mainfile
+ touch "$mdfile"
+ printf "## Manuscript\n\n" > "$mdfile"
+# echo "$(pandoc -t markdown ""$file"" --extract-media=img)" >> "$mdfile"
+ pandoc -t markdown "$file" --extract-media=img >> "$mdfile"
+ cat "$mdfile" > "$mainfile"
+ echo "$mainfile"
  pandoc "$mdfile" -o "$outfile" --latex-engine=xelatex -H $SETTINGS_DIR/options.sty
 fi
 done
 
 # Captions
-
-for file in $INPUT_DIR/$CAPTION_PREFIX*
+for file in "$INPUT_DIR/$CAPTION_PREFIX*"
 do
  if [ -f "$file" ] ; then
  echo "Processing $file"
- basename=$(basename "$file")
+ basename=$( echo $(basename "$file") |  sed 's/[^A-Za-z0-9_.]/-/g;s/-*-/-/g;s/^-//;s/-$//;s/-\././g' )
  mdfile="$OUTPUT_DIR/${basename%.*}.md"
  outfile="$OUTPUT_DIR/${basename%.*}.pdf"
  if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
  then
    echo "converting to docx"
-   unoconv -f docx "$file"
-   file="$INPUT_DIR/${basename%.*}.docx"
+   unoconv -f docx -o "$OUTPUT_DIR/${basename%.*}.docx" "$file"
+   file="$OUTPUT_DIR/${basename%.*}.docx"
  fi
- touch $mdfile
- printf "\n\n\pagebreak\n\n## Captions\n\n" > $mdfile
- printf "$(pandoc -t markdown ""$file"")" >> $mdfile
+ touch "$mdfile"
+ printf "\n\n\pagebreak\n\n## Captions\n\n" > "$mdfile"
+ printf "$(pandoc -t markdown ""$file"")" >> "$mdfile"
  pandoc "$mdfile" -o "$outfile" --latex-engine=xelatex -H $SETTINGS_DIR/options_pn.sty
- cat $mdfile >> $mainfile
+ cat "$mdfile" >> "$mainfile"
 fi
 done
 
@@ -60,15 +61,15 @@ do
  if [ -f "$file" ] ; then
  let COUNTER=COUNTER+1 
  echo "Processing $file"
- basename=$(basename "$file")
+ basename=$( echo $(basename "$file") |  sed 's/[^A-Za-z0-9_.]/-/g;s/-*-/-/g;s/^-//;s/-$//;s/-\././g' )
  htmlfile="$OUTPUT_DIR/${basename%.*}.html"
  outfile_pdf="$OUTPUT_DIR/${basename%.*}.pdf"
 # printf "\n\pagebreak\n\n# Table $((i+1))\n\n" > $outfile
  if [[ $(xdg-mime query filetype "$file") == application\/msword ]]
  then
    echo "converting to docx"
-   unoconv -f docx "$file"
-   file="$INPUT_DIR/${basename%.*}.docx"
+   unoconv -f docx -o "$OUTPUT_DIR/${basename%.*}.docx" "$file"
+   file="$OUTPUT_DIR/${basename%.*}.docx"
  fi
  pandoc "$file" -o "$htmlfile"
  pandoc "$htmlfile" -o "$outfile_pdf" --latex-engine=xelatex -H "$SETTINGS_DIR"/options_table.sty
@@ -76,13 +77,13 @@ do
  touch "$outfile_md"
  printf "\n\n\pagebreak\n\n## Table  $COUNTER: ${basename:5}\n\n" > "$outfile_md"
  mkdir "$outfile_pdf.dir"
- pdftk "$outfile_pdf" burst output "$outfile_pdf.dir"/page_%03d.pdf
- rm "$outfile_pdf.dir"/doc_data.txt
+ pdftk "$outfile_pdf" burst output "$outfile_pdf.dir/page_%03d.pdf"
+ rm "$outfile_pdf.dir/doc_data.txt"
  for file in "$outfile_pdf.dir"/*
  do
   printf "![$basename]($file)\ \n\n\pagebreak" >> "$outfile_md"
  done
- cat "$outfile_md" >> $mainfile
+ cat "$outfile_md" >> "$mainfile"
 fi
 done
 
@@ -93,18 +94,18 @@ do
 if [ -f "$file" ] ; then
  let COUNTER=COUNTER+1 
  echo "Processing $file"
- basename=$(basename "$file")
+ basename=$( echo $(basename "$file") |  sed 's/[^A-Za-z0-9_.]/-/g;s/-*-/-/g;s/^-//;s/-$//;s/-\././g' )
  outfile_img="$OUTPUT_DIR/${basename%.*}.pdf"
  $(convert "$file" $outfile_img)
  outfile_md="$OUTPUT_DIR/${basename%.*}.md"
  touch $outfile_md
- printf "\n\n\pagebreak\n\n## Figure $COUNTER: ${basename:5}\n\n" > $outfile_md
- printf "![$basename]($outfile_img)\ " >> $outfile_md
- cat $outfile_md >> $mainfile
+ printf "\n\n\pagebreak\n\n## Figure $COUNTER: ${basename:5}\n\n" > "$outfile_md"
+ printf "![$basename]($outfile_img)\ " >> "$outfile_md"
+ cat "$outfile_md" >> "$mainfile"
 fi
 done
 
 # re-pdf
 #pandoc $mainfile -o $OUTPUT_DIR/$(basename $mainfile).pdf --latex-engine=xelatex -H $SETTINGS_DIR/options.sty 
-pandoc $mainfile -o "$FINAL_OUTPUT_DIR"/review_pdf.pdf --latex-engine=xelatex -H $SETTINGS_DIR/options_pn.sty 
+pandoc "$mainfile" -o "$FINAL_OUTPUT_DIR"/review_pdf.pdf --latex-engine=xelatex -H $SETTINGS_DIR/options_pn.sty 
 tar -cf "$FINAL_OUTPUT_DIR"/output_archiv.tar "$OUTPUT_DIR"/ 
